394f161e92f4d6b70068d95278f90b3dc81d9a12
Tongxi Lou <tongxi.lou@gmail.com>
2025-01-17 00:55:20 +0100

    add testing for repeating element, leider noch keinen Fix

--- test_pcd_loader.py
+++ test_pcd_loader.py
@@ -5,22 +5,67 @@ import pytest
 
 from pcd_loader import PcdLoader
 
-POINTS = np.array(
-    [
-        [0, 0, 0],
-        [1, 0, 0],
-        [0, 1, 0],
-        [0, 0, 1],
-    ],
-    dtype=np.float64,
-)
+TEST_DATA = {
+    "simple": {
+        "data": np.array(
+            [
+                [0, 0, 0],
+                [1, 0, 0],
+                [0, 1, 0],
+                [0, 0, 1],
+            ],
+            dtype=np.float64,
+        ),
+        "ascii": """# .PCD v0.7 - Point Cloud Data file format
+VERSION 0.7
+FIELDS x y z
+SIZE 4 4 4
+TYPE F F F
+COUNT 1 1 1
+WIDTH 4
+HEIGHT 1
+VIEWPOINT 0 0 0 1 0 0 0
+POINTS 4
+DATA ascii
+0 0 0
+1 0 0
+0 1 0
+0 0 1
+""",
+    },
+    "repeating": {
+        "data": np.array(
+            [
+                [0, 0, 1],
+            ]
+            * 1000,
+            dtype=np.float64,
+        ),
+        "ascii": """# .PCD v0.7 - Point Cloud Data file format
+VERSION 0.7
+FIELDS x y z
+SIZE 4 4 4
+TYPE F F F
+COUNT 1 1 1
+WIDTH 4
+HEIGHT 1
+VIEWPOINT 0 0 0 1 0 0 0
+POINTS 4
+DATA ascii
+"""
+        + """0 0 1
+"""
+        * 1000,
+    },
+}
 
 
-def create_test_pointcloud(type_name: str) -> str:
+def create_test_pointcloud(type_name: str, data_name: str) -> str:
     """Fixture for creating fixed pointcloud.
 
     Args:
         type_name: whether the target is ascii or binary or binary_compressed
+        data_name: name of the test data (in TEST_DATA)
     """
     print(f"start with {type_name}")
     setting_save = {
@@ -39,7 +84,7 @@ def create_test_pointcloud(type_name: str) -> str:
 
     filename = f"./{type_name}.pcd"
     pc = o3d.geometry.PointCloud()
-    pc.points = o3d.utility.Vector3dVector(POINTS)
+    pc.points = o3d.utility.Vector3dVector(TEST_DATA[data_name]["data"])
 
     print(f"save {pc} in {filename}")
     o3d.io.write_point_cloud(filename, pc, **setting_save[type_name])
@@ -47,20 +92,21 @@ def create_test_pointcloud(type_name: str) -> str:
     return filename
 
 
-def check_values(filename: str):
+def check_values(filename: str, data_name: str):
     """Check whether the data is as expected.
 
     The point cloud points are predefined, it must be the same.
 
     Args:
         filename: input filename
+        data_name: name of the test data (in TEST_DATA)
     """
     pc = o3d.io.read_point_cloud(filename)
     points = np.asarray(pc.points)
-    assert np.abs(points - POINTS).max() < 1e-6
+    assert np.abs(points - TEST_DATA[data_name]["data"]).max() < 1e-6
 
 
-def check_ascii_file(filename: str):
+def check_ascii_file(filename: str, data_name: str):
     """Check whether the converted ascii is as expected.
 
     If fails assertion when the content is not the same; during checking,
@@ -68,29 +114,12 @@ def check_ascii_file(filename: str):
 
     Args:
         filename: input filename
+        data_name: name of the test data (in TEST_DATA)
     """
-    answer = """# .PCD v0.7 - Point Cloud Data file format
-VERSION 0.7
-FIELDS x y z
-SIZE 4 4 4
-TYPE F F F
-COUNT 1 1 1
-WIDTH 4
-HEIGHT 1
-VIEWPOINT 0 0 0 1 0 0 0
-POINTS 4
-DATA ascii
-0 0 0
-1 0 0
-0 1 0
-0 0 1
-"""
     with open(filename, "r", encoding="utf-8") as fi:
         content = fi.read().replace(".0 ", " ").replace(".0\n", "\n")
 
-    print(content)
-    print(answer)
-    assert content.strip() == answer.strip()
+    assert content.strip() == TEST_DATA[data_name]["ascii"].strip()
 
 
 @pytest.mark.parametrize(
@@ -101,18 +130,26 @@ DATA ascii
         ("binary_compressed"),
     ],
 )
-def test_type(type_name: str):
+@pytest.mark.parametrize(
+    "data_name",
+    [
+        ("simple"),
+        ("repeating"),
+    ],
+)
+def test_type(type_name: str, data_name: str):
     """Check for one pcd type.
 
     Args:
         type_name: whether the target is ascii or binary or binary_compressed
+        data_name: name of the test data (in TEST_DATA)
     """
-    filename = create_test_pointcloud(type_name=type_name)
+    filename = create_test_pointcloud(type_name=type_name, data_name=data_name)
 
     loader = PcdLoader(filename)
 
-    loader.save_ascii("test_ascii.pcd")
-    check_ascii_file("test_ascii.pcd")
+    loader.save_ascii(filename="test_ascii.pcd")
+    check_ascii_file(filename="test_ascii.pcd", data_name=data_name)
 
-    check_values(filename)
-    check_values("test_ascii.pcd")
+    check_values(filename=filename, data_name=data_name)
+    check_values(filename="test_ascii.pcd", data_name=data_name)
